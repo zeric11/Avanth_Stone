32030b2716c5846b7e5d36d037ba50bbac9cac88
Eric Zmitrovich <ezmitrovich11@gmail.com>
2023-10-27 20:34:22 -0700

    Completed player movement and ability controls
    Movement, attack, and block have been animated.

--- src/level.py
+++ src/level.py
@@ -1,13 +1,27 @@
 import pygame
 from maps import *
 from player import Player
+<<<<<<< Updated upstream
 from tile import Tile
 
 
+=======
+from enemy import Enemy
+from tile import Tile
+
+
+def get_surface():
+    zoom = 1
+    #return pygame.Surface((int(1000 / zoom), int(800 / zoom)))
+    return pygame.display.get_surface()
+
+
+>>>>>>> Stashed changes
 class Level:
     def __init__(self):
 
         # get the display surface
+<<<<<<< Updated upstream
         self.display_surface = pygame.display.get_surface()
 
         # Sprite group setup
@@ -21,12 +35,74 @@ class Level:
         # update and draw the game
         self.visible_sprites.draw(self.display_surface)
         self.visible_sprites.update()
+=======
+        self.display_surface = get_surface()
+
+        # Sprite group setup
+        self.visible_sprites = YSortCameraGroup()
+        self.obstacle_sprites = pygame.sprite.Group()
+
+        # attack sprites
+        self.current_attack = None
+        self.attack_sprites = pygame.sprite.Group()
+        self.attackable_sprites = pygame.sprite.Group()
+
+        # sprite setup
+        self.create_map()
+
+
+    def run(self):
+        # update and draw the game
+        self.visible_sprites.custom_draw(self.player)
+        self.visible_sprites.update()
+        self.visible_sprites.enemy_update(self.player)
+
+>>>>>>> Stashed changes
 
     def create_map(self):
         for i in range(len(MAP_0)):
             for j in range(len(MAP_0[0])):
                 item = MAP_0[i][j]
                 if item == "p":
+<<<<<<< Updated upstream
                     self.player = Player((j * TILE_SIZE, i * TILE_SIZE), [self.visible_sprites], self.obstacle_sprites)
                 elif item == "x":
                     Tile((j * TILE_SIZE, i * TILE_SIZE), [self.visible_sprites, self.obstacle_sprites])
+=======
+                    self.player = Player((j * TILE_SIZE, i * TILE_SIZE), 
+                                         [self.visible_sprites], 
+                                         self.obstacle_sprites)
+                elif item == "g":
+                    Enemy("ghost", 
+                          (j * TILE_SIZE, i * TILE_SIZE), 
+                          [self.visible_sprites, self.attackable_sprites], 
+                          self.obstacle_sprites)
+                elif item == "x":
+                    Tile((j * TILE_SIZE, i * TILE_SIZE), 
+                         [self.visible_sprites, self.obstacle_sprites], 
+                         "rock")
+
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+
+
+    def custom_draw(self, player):
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
+
+
+    def enemy_update(self, player):
+        enemy_sprites = [sprite for sprite in self.sprites() if hasattr(sprite, "sprite_type") and sprite.sprite_type == "enemy"]
+        for enemy in enemy_sprites:
+            enemy.enemy_update(player)
+>>>>>>> Stashed changes
--- src/tile.py
+++ src/tile.py
@@ -2,7 +2,17 @@ import pygame
 
 
 class Tile(pygame.sprite.Sprite):
+<<<<<<< Updated upstream
     def __init__(self, pos, groups):
         super().__init__(groups)
         self.image = pygame.image.load("../graphics/nature/rock.png").convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
+=======
+    def __init__(self, pos, groups, sprite_type):
+        super().__init__(groups)
+        self.sprite_type = sprite_type
+        self.image = pygame.image.load("../textures/nature/rock.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (64, 64))
+        self.rect = self.image.get_rect(topleft=pos)
+        self.hitbox = self.rect.inflate(0, -10)
+>>>>>>> Stashed changes
